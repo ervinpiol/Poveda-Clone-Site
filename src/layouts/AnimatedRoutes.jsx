55cec63dc59d46c687f293b1609674bd0f3f5ebe
Ervin Piol <112147440+ervinpiol@users.noreply.github.com>
2023-09-24 19:37:49 +0800

    Update AnimatedRoutes.jsx

--- src/layouts/AnimatedRoutes.jsx
+++ src/layouts/AnimatedRoutes.jsx
@@ -14,7 +14,7 @@ const AnimatedRoutes = () => {
   const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    window.scrollTo(0, 0);
   }, [location]);
 
   return (
